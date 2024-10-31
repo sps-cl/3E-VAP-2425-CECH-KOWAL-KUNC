08c6143e7eeb929080c3f23bd71aa2ce392de83a
Radim Kunc <radim.kunc@student.sps-cl.cz>
2024-11-01 00:23:40 +0100

    css + základ generování čísel

---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+
+// https://stackoverflow.com/questions/10058226/send-response-to-all-clients-except-sender#answer-10099325
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('btnClick', (data) => {
+    console.log(data);
+    io.emit('btnClick', data + 1);
+  });
+
+  socket.on('JMENO_CLIENT_TO_SERVER_EVENTU', (data) => {
+    io.emit('JMENO_SERVER_TO_CLIENT_EVENTU', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
